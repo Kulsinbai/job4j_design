8826c250b22ad478e6f91ec798f065c80c7be114
kulsinbai <mcc.kulsinbai@gmail.com>
2026-03-14 09:12:48 +0500

    7. Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,21 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        int index = 0;
+
+        while (source.hasNext()) {
+            Integer value = source.next();
+            nodes.get(index).add(value);
+            index++;
+
+            if (index == nodes.size()) {
+                index = 0;
+            }
+        }
+    }
+}
